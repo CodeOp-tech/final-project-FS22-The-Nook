8b3d099b85e4bf2b97100c90c78df1243582a684
Veronica <veronica_baldin@live.it>
2022-11-11 11:09:13 +0100

    Login functions and initial view

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
 
+import Local from './helpers/Local';
+import Api from './helpers/Api';
+
+import LoginView from './views/LoginView';
+
+
 function App() {
+  const [user, setUser] = useState(Local.getUser());
+  const [loginErrorMsg, setLoginErrorMsg] = useState('');
+  const navigate = useNavigate();
+
+  async function doLogin(username, password) {
+    let myresponse = await Api.loginUser(username, password);
+    if (myresponse.ok) {
+        Local.saveUserInfo(myresponse.data.token, myresponse.data.user);
+        setUser(myresponse.data.user);
+        setLoginErrorMsg('');
+        navigate('/');
+    } else {
+        setLoginErrorMsg('Login failed');
+    }
+}
+
+  function doLogout() {
+    Local.removeUserInfo();
+    setUser(null);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/login" element={
+          <LoginView 
+            loginCb={(u, p) => doLogin(u, p)} 
+            loginError={loginErrorMsg} 
+            />
+         } />
+      </Routes>
     </div>
   );
 }
