9e2732067f2983ac9f3552566e5384499a3ada07
Lauren Goldstein <laurengolds@gmail.com>
2022-11-15 15:59:01 +0100

    formatted date on bookshelf cards

--- client/src/components/ClubBookshelf.js
+++ client/src/components/ClubBookshelf.js
@@ -9,6 +9,27 @@ function ClubBookshelf(props) {
     fetchClubBooks();
   }, []);
 
+  function formatDate(date) {
+    const months = {
+      "01": "January",
+      "02": "February",
+      "03": "March",
+      "04": "April",
+      "05": "May",
+      "06": "June",
+      "07": "July",
+      "08": "August",
+      "09": "September",
+      10: "October",
+      11: "November",
+      12: "December",
+    };
+    let year = date.slice(0, 4);
+    let month = months[date.slice(5, 7)];
+    let day = date.slice(8, 10);
+
+    return `${month} ${day}, ${year}`;
+  }
   //Get clubs's books
   async function fetchClubBooks() {
     let myresponse = await Api.getClubBooks(`${props.club.id}`);
@@ -23,7 +44,7 @@ function ClubBookshelf(props) {
   }
 
   return (
-    <div className="ClubBookshelf">
+    <div className="ClubBookshelf row mb-3">
       {clubBooks.map((b) => (
         <div key={b.id} className="col my-2">
           <div className="card" style={{ width: "18rem" }}>
@@ -38,7 +59,7 @@ function ClubBookshelf(props) {
               <div className="card-body">
                 <h5 className="card-title">{b.title}</h5>
                 <h6 className="card-text">By {b.author}</h6>
-                <h6 className="card-text">Discussed {b.date.slice(0, 7)}</h6>
+                <h6 className="card-text">Discussed {formatDate(b.date)}</h6>
               </div>
             </div>
           </div>
