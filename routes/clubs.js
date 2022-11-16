b12a18bb9d438120c769f1bc3774a38bb38d4f87
Lauren Goldstein <laurengolds@gmail.com>
2022-11-16 14:06:24 +0100

    PATCH route in clubs functioning in Postman

--- routes/clubs.js
+++ routes/clubs.js
@@ -45,4 +45,32 @@ router.get("/:id", async function (req, res) {
   }
 });
 
+// Update the next meeting time and location
+router.patch("/:id", async function (req, res) {
+  let sql = `
+      UPDATE clubs
+      SET
+        next_mtg_time = '${req.body.next_mtg_time}',
+        next_mtg_location_name = '${req.body.next_mtg_location_name}',
+        next_mtg_address = '${req.body.next_mtg_address}',
+        next_mtg_city = '${req.body.next_mtg_city}',
+        next_mtg_postal_code = '${req.body.next_mtg_postal_code}',
+        next_mtg_country = '${req.body.next_mtg_country}'
+      WHERE
+        id = ${req.params.id};
+    `;
+  try {
+    let club = await db(`SELECT * FROM clubs WHERE id = ${req.params.id}`);
+    if (club.data.length === 0) {
+      res.status(404).send({ error: "Club does not exist." });
+    } else {
+      await db(sql);
+      let result = await db(`SELECT * FROM clubs WHERE id = ${req.params.id}`);
+      res.status(201).send(result.data);
+    }
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+});
+
 module.exports = router;
