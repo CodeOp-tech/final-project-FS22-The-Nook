ec4c7d7f4b6b83f63238fa7190b535d8b06e5154
Lauren Goldstein <laurengolds@gmail.com>
2022-11-22 11:32:59 +0100

    added CreateClub drowdown menu to ClubSearch category dropdown menu

--- client/src/components/ClubSearch.js
+++ client/src/components/ClubSearch.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import categoryList from "./DropdownCategoryList.js";
 
 function ClubSearch(props) {
   const nav = useNavigate();
@@ -45,8 +46,8 @@ function ClubSearch(props) {
           onChange={(e) => setSearchLocation(e.target.value)}
         />
 
-        <div className="mt-3">
-          {/* <label htmlFor="floatingSelect">Choose a category</label> */}
+        {/* <div className="mt-3">
+          <label htmlFor="floatingSelect">Choose a category</label>
           <select
             className="form-select"
             id="floatingSelect"
@@ -57,22 +58,35 @@ function ClubSearch(props) {
             <option className="" defaultValue>
               Choose a category
             </option>
-            <option data-tokens="1">adventure</option>
-            <option data-tokens="2">biography</option>
-            <option data-tokens="3">casual</option>
-            <option data-tokens="4">childrens literature</option>
-            <option data-tokens="5">classics</option>
-            <option data-tokens="6">fantasy</option>
-            <option data-tokens="7">graphic novels</option>
-            <option data-tokens="8">historical fiction</option>
-            <option data-tokens="9">mystery</option>
-            <option data-tokens="10">non-fiction</option>
-            <option data-tokens="11">other</option>
-            <option data-tokens="12">romance</option>
-            <option data-tokens="13">sci-fi</option>
-            <option data-tokens="14">travel</option>
+            <option data-tokens="1">biography</option>
+            <option data-tokens="2">casual</option>
+            <option data-tokens="3">childrens literature</option>
+            <option data-tokens="4">classics</option>
+            <option data-tokens="5">historical fiction</option>
+            <option data-tokens="6">sci-fi</option>
+            <option data-tokens="7">travel</option>
+          </select>
+        </div> */}
+
+        <div className="mb-3 dropdown">
+          <select
+            className="form-select"
+            id="floatingSelect"
+            name="category"
+            aria-label="Choose category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option defaultValue>Choose a category</option>
+            {categoryList.map((c) => (
+              <option className="dropdown-item" key={c} value={c}>
+                {c}
+              </option>
+            ))}
+            ;
           </select>
         </div>
+
         <button className="btn btn-outline-secondary mt-3" type="submit">
           Search
         </button>
