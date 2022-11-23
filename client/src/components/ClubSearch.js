329a98f525d6053fdcb3e73bd5d0344915f5ba87
Antonia <antoniaioanavladut@gmail.com>
2022-11-23 15:52:15 +0200

    Contact functional via email, Error view w buttons

--- client/src/components/ClubSearch.js
+++ client/src/components/ClubSearch.js
@@ -16,7 +16,7 @@ function ClubSearch(props) {
 
     function changeUrl(){
 
-      navigate(`/clubs//?search=${searchInput}&location=${searchLocation}&category=${category}`)
+      navigate(`/clubs/?search=${searchInput}&location=${searchLocation}&category=${category}`)
       props.getClubs && props.getClubs()
       
     }
